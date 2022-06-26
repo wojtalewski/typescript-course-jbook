147ee4a58a604a17a719fdffd3d6d7360a2b5673
Łukasz Wojtalewicz <l.wojtalewicz@yahoo.com>
2022-06-26 20:46:03 +0200

    new repo

---
+++ packages/local-client/src/components/preview.tsx
@@ -0,0 +1,63 @@
+import './preview.css'
+import { useEffect, useRef } from 'react'
+
+interface PreviewProps {
+  code: string
+  err: string
+}
+
+const html = `
+<html>
+  <head>
+    <style>html { background-color: white }</style>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script>
+    const handleError = (error) => {
+      const root = document.querySelector('#root')
+      root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + error + '</div>'
+      console.error(error)
+    }
+
+    window.addEventListener('error', (event) => {
+      event.preventDefault()
+      handleError(event.error)
+    })
+
+    window.addEventListener('message', (event) => {
+      try {
+        eval(event.data)
+      } catch (error) {
+        handleError(error)
+      }
+    }, false)
+    </script>
+  </body>
+</html>
+`
+
+const Preview: React.FC<PreviewProps> = ({ code, err }) => {
+  const iframe = useRef<any>()
+
+  useEffect(() => {
+    iframe.current.srcdoc = html
+    setTimeout(() => {
+      iframe.current.contentWindow.postMessage(code, '*')
+    }, 50)
+  }, [code])
+
+  return (
+    <div className='preview-wrapper'>
+      <iframe
+        title='preview'
+        ref={iframe}
+        sandbox='allow-scripts'
+        srcDoc={html}
+      />
+      {err && <div className='preview-error'>{err}</div>}
+    </div>
+  )
+}
+
+export default Preview
