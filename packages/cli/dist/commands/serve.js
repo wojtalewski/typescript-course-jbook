a22dfdb15b77059077a7d1857ca5d486d9f3bf50
Łukasz Wojtalewicz <l.wojtalewicz@yahoo.com>
2022-06-29 17:55:52 +0200

    final commit

--- packages/cli/dist/commands/serve.js
+++ packages/cli/dist/commands/serve.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,12 +15,27 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.serveCommand = void 0;
 const path_1 = __importDefault(require("path"));
 const commander_1 = require("commander");
-const local_api_1 = require("local-api");
+const local_api_1 = require("@jsnote-wojtalewicz/local-api");
+const isProduction = process.env.NODE_ENV === 'production';
 exports.serveCommand = new commander_1.Command()
     .command('serve [filename]')
     .description('Open a file for editing')
     .option('-p --port <number>', 'port to run server on', '4005')
-    .action((filename = 'notebook.js', options) => {
-    const dir = path_1.default.join(process.cwd(), path_1.default.dirname(filename));
-    (0, local_api_1.serve)(+options.port, path_1.default.basename(filename), dir);
-});
+    .action((filename = 'notebook.js', options) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const dir = path_1.default.join(process.cwd(), path_1.default.dirname(filename));
+        yield (0, local_api_1.serve)(+options.port, path_1.default.basename(filename), dir, !isProduction);
+        console.log(`Opened ${filename}. Navigate to http://localhost:${options.port} to edit the file`);
+    }
+    catch (error) {
+        // @ts-ignore
+        if (error.code === 'EADDRINUSE') {
+            console.error('Port is in use. Try running on a different port');
+        }
+        else {
+            // @ts-ignore
+            console.log('Heres the problem', error.message);
+        }
+        process.exit(1);
+    }
+}));
